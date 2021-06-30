051ac01b8e4ede8bac204da469006e0876b3f29a
NTIVUGURUZWA Emmanuel <djntivuguruzwaemmanuel@gmail.com>
2021-06-30 06:02:57 +0000

    app updates

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,8 @@ mongoose.connect(process.env.MONGO_URL, ({
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 })).then(()=>{
     console.log('Connected to Database')
 }).catch(()=>{
